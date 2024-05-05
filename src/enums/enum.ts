062af67d5e9b2cc0964868c957ad734bb5243bb0
Leof098 <70485779+Leof098@users.noreply.github.com>
2024-05-05 18:10:48 -0300

    first commit

---
+++ src/enums/enum.ts
@@ -0,0 +1,52 @@
+export enum HeroSectionEnum {
+  TITLE = "Meet $NALA",
+  SUB_TITLE = "Shelter Cat's",
+  BUTTON = "Get to know me!",
+}
+
+export enum LandingHeaderEnum {
+  SECTION_1 = "Section 1",
+  SECTION_2 = "Section 2",
+  SECTION_3 = "Section 3",
+  SECTION_4 = "Section 4",
+  HOME = "Home",
+  CONTRACT = "Contract",
+  ABOUT = "About",
+  TWITTER = "Twitter",
+  TELEGRAM = "Telegram",
+  INSTAGRAM = "Instagram",
+}
+
+export enum SectionOneEnum {
+  TITLE = "Model S",
+  SUB_TITLE = "Placer de conduccion",
+  BUTTON_1 = "Personalizar",
+  BUTTON_2 = "Ver inventario",
+}
+
+export enum SectionTwoEnum {
+  TITLE = "Model 3",
+  SUB_TITLE = "Ordenar en linea para entrega sin contacto",
+  BUTTON_1 = "Personalizar",
+  BUTTON_2 = "Ver inventario",
+}
+
+export enum SectionThreeEnum {
+  TITLE = "Model X",
+  SUB_TITLE = "Ordenar en linea para entrega sin contacto",
+  BUTTON_1 = "Personalizar",
+  BUTTON_2 = "Ver inventario",
+}
+
+export enum ContractSectionEnum {
+  CONTRACT = "CEjemploDeContratoTVSDoxmGGsWppy6Vhxkn",
+  TEXT_1 = "Contrato de $NALA",
+  BUTTON_1 = "Personalizar",
+  BUTTON_2 = "Ver inventario",
+}
+
+export enum UrlsEnum {
+  TWITTER = "https://twitter.com/Nala_Cat_Coin",
+  TELEGRAM = "https://t.me/",
+  INSTAGRAM = "https://www.instagram.com/nala_cat/",
+}
